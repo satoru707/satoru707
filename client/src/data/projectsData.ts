30a6284a7310146ae6f13de95afc358e81f9631b
satoru707 <petolulope7@gmail.com>
2025-09-21 14:05:08 +0100

    experience added

--- client/src/data/projectsData.ts
+++ client/src/data/projectsData.ts
@@ -11,16 +11,6 @@ export const projects: Project[] = [
     liveUrl: "https://timeforge-eight.vercel.app",
     image: "./timeforge.png",
   },
-  // {
-  //   id: 2,
-  //   title: "CollabCode",
-  //   description:
-  //     "Developer collaboration platform with real-time code sharing and review capabilities.",
-  //   stack: ["Node.js", "Socket.io", "Express", "MongoDB", "React"],
-  //   liveUrl: "https://collab-code-mauve.vercel.app",
-  //   githubUrl: "https://github.com/satoru707/CollabCode",
-  //   image: "./collabcode.png",
-  // },
   {
     id: 2,
     title: "TaskNest",
@@ -30,5 +20,35 @@ export const projects: Project[] = [
     githubUrl: "https://github.com/satoru707/TaskNest",
     liveUrl: "https://task-nest-gamma-eight.vercel.app",
     image: "./tasknest.png",
+  },
+  {
+    id: 3,
+    title: "MemoryLane",
+    description:
+      "Memory Lane is an AI-powered personal timeline app that helps you remember important moments and events from your life.",
+    stack: ["Nextjs", "TypeScript", "Dexie", "Next-Auth", "Drizzle ORM"],
+    githubUrl: "https://github.com/satoru707/MEMORY_LANE",
+    liveUrl: "https://memory-lane-chi.vercel.app/",
+    image: "./memory.png",
+  },
+  {
+    id: 4,
+    title: "OctoSustain",
+    description:
+      "A Real-Time Eco-Collaboration Platform to Track your eco-impact with OctoPods. Multitask like an octopus and save the planet!",
+    stack: ["Nextjs", "TypeScript", "JWT", "Recharts", "Prisma ORM"],
+    githubUrl: "https://github.com/satoru707/OctoSustain",
+    liveUrl: "https://octo-sustain.vercel.app",
+    image: "./octo.png",
+  },
+  {
+    id: 5,
+    title: "I-VOTEE",
+    description:
+      "A comprehensive, secure e-voting platform designed specifically for universities with cryptographic integrity verification, role-based access control, and mobile-first design.",
+    stack: ["React", "Nodejs", "TypeScript", "JWT", "Crypto", "Prisma ORM"],
+    githubUrl: "https://github.com/satoru707/voting_app",
+    liveUrl: "https://voting-app-topaz.vercel.app/",
+    image: "./vote.png",
   },
 ];
